205859cad59a32aff7eef594855c3b3efbb60230
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-03-17 00:23:50 +0900

    Delete codebases for HipChat integration

--- src/dajare/helpers.rb
+++ src/dajare/helpers.rb
@@ -7,42 +7,5 @@ module Dajare
     def h(str)
       CGI.escape_html(str.to_s)
     end
-
-    def descriptor
-      {
-        name: "Dajare",
-        description: "An integration for making your rooms cool.",
-        key: "com.satoryu.dajare",
-        links: {
-          homepage: base_url,
-          self: "#{base_url}/descriptor"
-        },
-        capabilities: {
-          hipchatApiConsumer: {
-            avatar: {
-              url: ENV['HIPCHAT_AVATAR_URL']
-            },
-            scopes: %w[send_message view_messages]
-          },
-          installable: {
-            allowGlobal: true,
-            allowRoom: true,
-          },
-          webhook: [
-            {
-              name: "simple dajarize",
-              key: 'simpel_dajarizer',
-              url: "#{base_url}/webhook",
-              event: 'room_message',
-              pattern: "^.+$",
-            }
-          ]
-        }
-      }
-    end
-
-    def base_url
-      "#{request.scheme}://#{request.host}"
-    end
   end
 end
